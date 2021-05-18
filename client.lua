2b513158b7ddddc908567b209797b87fbb86b95d
cadburry <72443203+cadburry6969@users.noreply.github.com>
2021-05-19 00:27:25 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
 RegisterNetEvent('cc-notify')
 AddEventHandler('cc-notify', function(text, color, time)
-	SendNUIMessage({text = text, color = color or 1, time = time or 10000})
+	SendNUIMessage({text = text, color = color or 'default', time = time or 10000})
 end)
